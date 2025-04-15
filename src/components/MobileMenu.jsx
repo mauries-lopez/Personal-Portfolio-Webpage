68bb9c4785215a8ce8c6d50a1fa4cb4bb8f7c479
mauries-lopez <102708347+mauries-lopez@users.noreply.github.com>
2025-04-15 15:26:52 +0800

    Main Menu Feature Commit
    - Animations for Main Menu
    - Added lists of sections in Main Menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,25 +1,46 @@
+import { useState } from "react";
 
 export const MobileMenu = ({curHamburgerStatus, setCurHamburgerStatus}) => {
 
-    function handleHamburgerClick(){
-        setCurHamburgerStatus(false);
+    const [animateClass, setAnimateClass] = useState("animate-div-slide-out");
+    const [animateBlurryDivClass, setAnimateBlurryDiv] = useState("animate-blurryDiv-fade-in");
+
+    function handleHamburgerClick() {
+        setAnimateClass("animate-div-slide-in"); 
+        setAnimateBlurryDiv("animate-blurryDiv-fade-out");
+        setTimeout(() => {
+            setCurHamburgerStatus(false);
+        }, 750);
     }
 
     return (
-        <div className="absolute h-dvh sm:w-[100%] md:w-[50%] bg-blue-700 text-white flex flex-col">
-            <div id="upperPortion" className="relative h-[20%] w-full flex flex-row justify-start items-center transform transition-transform duration-500 ease-in-out translate-x-0">
-                <div className="relative left-[4rem] transform transition-transform duration-200 hover:scale-125 flex flex-row space-x-1.5 cursor-pointer" id="hamburgerBtn" onClick={handleHamburgerClick}>
-                    <div className="w-[0.3rem] h-[2rem] bg-white duration-300"></div>
-                    <div className="w-[0.3rem] h-[2rem] bg-white duration-300"></div>
-                    <div className="w-[0.34rem] h-[2rem] bg-white duration-300"></div>
+        <div className="absolute top-0 left-0 z-50 h-dvh w-dvw flex flex-row">
+            <div id="mainMenu" className={`relative h-dvh 3xs:w-[100%] md:w-[50%] bg-white text-black flex flex-col ${animateClass} bg-no-repeat bg-cover bg-center`} style={{backgroundImage: "url(/mainMenuBG.png"}}>
+                <div id="upperPortion" className="relative h-[20%] w-full flex flex-row justify-start items-center transform transition-transform duration-500 ease-in-out translate-x-0">
+                    <div className="relative left-[4rem] transform transition-transform duration-200 hover:scale-125 flex flex-row space-x-1.5 cursor-pointer" id="hamburgerBtn" onClick={handleHamburgerClick}>
+                        <span className="text-5xl"> X </span>
+                    </div>
+                </div>
+                <div id="middlePortion" className="relative h-[60%] w-full flex justify-center items-start">
+                    <div>
+                        <ul class="list-inside list-[upper-roman] text-3xl/17 font-bold">
+                            <li className="hover:underline cursor-pointer"> About Me </li>
+                            <li className="hover:underline cursor-pointer"> Projects </li>
+                            <li className="hover:underline cursor-pointer"> Tech Stack </li>
+                            <li className="hover:underline cursor-pointer"> Education </li>
+                            <li className="hover:underline cursor-pointer"> Experience </li>
+                            <li className="hover:underline cursor-pointer"> Contact </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="lowerPortion" className="relative h-[20%] w-full ">
+
                 </div>
             </div>
-            <div id="middlePortion" className="relative h-[60%] w-full bg-red-500">
-
-            </div>
-            <div id="lowerPortion" className="relative h-[20%] w-full bg-blue-600">
+            <div id="blurrySide" className={`hidden md:block relative h-dvh bg-black opacity-50 md:w-[50%] ${animateBlurryDivClass}`}>
 
             </div>
         </div>
+        
     );
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,22 +7,22 @@ export const NavigationBar = ({ curHamburgerStatus, setCurHamburgerStatus}) => {
     }
     
     return (
-        <nav className="absolute w-dvw h-[6rem] background-color: bg-black opacity-90 flex flex-row items-center">
+        <nav className="sticky top-0 z-40 w-dvw h-[6rem] background-color: bg-black opacity-90 flex flex-row items-center">
             <div className="relative h-full w-xs flex flex-row space-x-1.5 justify-center items-center" id="hamburgerPartition" >
                 <div className="transform transition-transform duration-200 hover:scale-125 flex flex-row space-x-1.5 cursor-pointer" id="hamburgerBtn" onClick={handleHamburgerClick}>
                     <div className="w-[0.3rem] h-[2rem] bg-white duration-300"></div>
                     <div className="w-[0.3rem] h-[2rem] bg-white duration-300"></div>
-                    <div className="w-[0.34rem] h-[2rem] bg-white duration-300"></div>
+                    <div className="w-[0.3rem] h-[2rem] bg-white duration-300"></div>
                 </div>
             </div>
             <div className="relative h-full w-full" id="titlePartition">
-                <div className="h-full w-full flex flex-row items-center justify-center text-3xl font-bold text-white text-center">
+                <div className="h-full w-full flex flex-row items-center justify-center sm:text-3xl 3xs:text-sm font-bold text-white text-center">
                     <span > Mauries Lopez </span>
                 </div>
             </div>
             <div className="relative h-full w-xs" id="socialMediaPartition">
                 <div className="h-full w-full flex flex-row items-center justify-center ">
-                    <img src={socialMediaLogo} alt="Social Media Logo" width="50" className="cursor-pointer transform transition-transform duration-200 hover:scale-125"/>
+                    <img src={socialMediaLogo} alt="Social Media Logo" width="50" height="50" className="cursor-pointer transform transition-transform duration-200 hover:scale-125"/>
                 </div>
             </div>
         </nav>
